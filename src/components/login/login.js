9a180dbdbca1683ef66c6a4860de27d3feed4216
juanchojjrc <juanchojjrc@gmail.com>
2020-09-14 17:48:29 -0400

    Arreglo Login V1

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -182,40 +182,11 @@ const getClienteById = (id) => {
                 Portafolio de Solicitudes
             </Typography>
             <form className={classes.form} method="post">
-                <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                />
-                <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                />
-                <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                >
-                Sign In
-                </Button>
+               
                
                 <Box mt={2} mb={2}>
                 <GoogleLogin
+                className={classes.login}
                 clientId={config.clientId}
                 buttonText="Iniciar Sesión"
                 onSuccess={responseGoogle}
